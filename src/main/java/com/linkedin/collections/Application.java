0c5f60b306cfaf585193aa49b9c6ef028026d4b5
Jihun Park <117942872+itsjihunpark@users.noreply.github.com>
2023-08-04 21:12:08 +0000

    Set concept and some of the new set methods like .of and .copyOf

--- src/main/java/com/linkedin/collections/Application.java
+++ src/main/java/com/linkedin/collections/Application.java
@@ -1,5 +1,7 @@
 package com.linkedin.collections;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
 public class Application {
 
 	public static void main(String[] args) {
@@ -9,6 +11,33 @@ public class Application {
 		Room oxfordDuplicate = new Room("Oxford", "Suite", 5, 225.0);
 		Room victoria = new Room("Victoria", "Suite", 5, 225.00);
 
-		
+		Set<Room> rooms = new LinkedHashSet<>(); 
+			//Order is not guarenteed
+			//If we want it to have order, then we need
+			//to define rooms as an instance of a linkedHashSet
+		rooms.add(piccadilly);
+		rooms.add(oxford);
+		rooms.add(oxford);
+		rooms.add(oxfordDuplicate);
+		rooms.add(victoria);
+		//Some new methods that were added to the set interface
+
+		Set<Room> otherRooms = Set.of(piccadilly,oxford); 
+		//.of method is used to create an unmodifiable Set instance
+		// This can be helpful for thread safety or sometimes performance
+		//.of method has several overloaded forms that can take in up to 9 elements
+
+		rooms.stream()
+			.map(r->r.getName())
+			.forEach(r->System.out.println(r));
+
+		//.copyOf
+		Set<Room> moreRooms = Set.copyOf(rooms); 
+		//unmodifiable set is backed by the first collection we copied
+		moreRooms.stream()
+			.map(r->r.getName())
+			.forEach(System.out::println);
+
+
 	}
 }
